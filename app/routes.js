cba744fc3aebabdd109b3cea07628c89b298bb01
Niel Eyde <niel.c.eyde@accenture.com>
2018-09-25 18:44:29 +0000

    removed logging

--- app/routes.js
+++ app/routes.js
@@ -12,23 +12,15 @@ var tableName = "tasks"
   , accountName = AccountName
   , accountKey = AccountKey;
 
-
-console.log("create table service");
-
 var tableService = azure.createTableService(accountName, accountKey);
-
-console.log("create table");
 
 tableService.createTableIfNotExists(tableName, function(error, result, response) {
   if (!error) {
     // result contains true if created; false if already exists
-    console.log(result);
   }
 });
 
 function getTasks(res) {
-
-    console.log("start get tasks");
 
     var query = new azure.TableQuery()
        .top(10)
@@ -36,15 +28,10 @@ function getTasks(res) {
 
     tableService.queryEntities(tableName, query, null, function(error, result, response) {
       if (error) {
-        console.log("start get tasks - ERROR");
         res.send(err);
         }
       if (!error) {
          // result.entries contains entities matching the query
-         console.log("start get tasks - RESULT");
-         console.log(result);
-         console.log("start get tasks - ETNRIES ONLY");
-         console.log(result.entries);
          res.json(result.entries);
       }
 });
@@ -55,14 +42,12 @@ module.exports = function (app) {
     // api ---------------------------------------------------------------------
     // get all todos
     app.get('/api/todos', function (req, res) {
-        console.log("app.get");
         // use mongoose to get all todos in the database
         getTasks(res);
     });
 
     // create todo and send back all todos after creation
     app.post('/api/todos', function (req, res) {
-        console.log("app.post");
 
         var entGen = azure.TableUtilities.entityGenerator;
         var entity = {
@@ -72,18 +57,12 @@ module.exports = function (app) {
           done: entGen.Boolean(false)
         };
         tableService.insertEntity(tableName, entity, function(error, result, response) {
-            console.log("result");
-            console.log(result);
-            console.log("response");
-            console.log(response);
 
             if (error) {
                 // result contains the ETag for the new entity
-                console.log("app.post error");
             }
                 if (!error) {
             // result contains the ETag for the new entity
-            console.log("app.post successfull");
         }
         getTasks(res);
     });
@@ -92,8 +71,6 @@ module.exports = function (app) {
     // delete a todo
 
     app.delete('/api/todos/:todo_id', function (req, res) {
-        console.log("delete");
-        console.log(req.params.todo_id);
 
         var entGen = azure.TableUtilities.entityGenerator;
         var entity = {
@@ -105,11 +82,9 @@ module.exports = function (app) {
 
             if (error) {
                 // result contains the ETag for the new entity
-                console.log("app.delete error");
             }
                 if (!error) {
             // result contains the ETag for the new entity
-            console.log("app.delete successfull");
         }
         getTasks(res);
 
